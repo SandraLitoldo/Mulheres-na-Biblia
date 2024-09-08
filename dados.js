465d3e9eb99738b7ce22eb7552944053722bbbef
SandraLitoldo <sandrapaulasanches@gmail.com>
2024-09-08 13:52:25 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,55 +1,55 @@
 let dados = [
     {
         personagem: "Eva",
-        historia: "Eva: A primeira mulher, segundo a narrativa bíblica e figura central na história da queda do homem.",
+        historia: "Eva:Principais Fatos: Eva foi a primeira mulher criada por Deus, conforme descrito no livro de Gênesis. Ela é conhecida por ser a esposa de Adão e a mãe de Caim, Abel e Sete. Eva é central na narrativa da Queda, quando desobedeceu a Deus ao comer o fruto proibido, levando à entrada do pecado no mundo.Localização Bíblica, Gênesis 2:18-25 e Gênesis 3.",
         link: "https://www.bibliaon.com/genesis_2/",
         tags: "mulher primeira homem"
     },
     {   
         personagem: "Sara",
-        historia: "Sara: Esposa de Abraão, símbolo de fé e perseverança, mesmo diante da infertilidade.",
-        link: "https://www.bibliaon.com/genesis_18",
+        historia: "Principais Fatos: Sara, esposa de Abraão, é conhecida por ter dado à luz a Isaque em sua velhice, após um período de infertilidade. Sua história é marcada pela promessa de Deus de que ela seria a mãe de muitas nações. Inicialmente, ela riu quando soube da promessa, mas mais tarde, cumpriu o papel de mãe da nação israelita. Localização Bíblica, Gênesis 11:29-30, Gênesis 17:15-19, e Gênesis 21:1-3.",
+        link: "https://www.bibliaon.com/genesis_11",
         tags: "Abraão fé infertilidade"
     }, 
     {
         personagem: "Rebeca", 
-        historia: "A história de Rebeca é contada principalmente no livro de Gênesis,no Antigo Testamento da Bíblia. Ela é escolhida para ser a esposa de Isaque, filho de Abraão, e desempenha um papel crucial na linhagem que levaria ao nascimento de Jesus.",
+        historia: "Principais Fatos: Rebeca foi escolhida para ser esposa de Isaque, filho de Abraão. Ela é conhecida por sua hospitalidade e pela sua decisão de deixar sua família para se casar com Isaque. Rebeca também é a mãe de Jacó e Esaú. Localização Bíblica Gênesis 24 e Gênesis 25:19-28.",
         link: "https://www.bibliaon.com/genesis_24/",
         tags: "Jaco Isaque "
     },
     {
         personagem:  "Raabe",
-        historia: "Uma prostituta cananeia que ajudou os espias israelitas e foi abençoada por Deus, tornando-se parte do povo de Israel.",
+        historia: "Principais Fatos: Raabe era uma mulher de Jericó que escondeu os espias israelitas enviados por Josué. Sua coragem e fé em Deus resultaram na preservação de sua vida e da de sua família durante a conquista de Jericó. Raabe é a mãe de Boaz e bisavó de Jesus. Localização Bíblica: Josué 2 e Josué 6:22-25. Ela é também mencionada em Mateus 1:5 na genealogia de Jesus.",
         link:" https://www.bibliaon.com/josue_2/",
         tags: "espias Josué"
     },
     {
-        personagem: "Rute",
-        historia: "Uma moabita que escolheu seguir a sogra, Rute, e se tornou parte do povo de Israel, sendo avó do rei Davi.",
+        personagem: "Rute", 
+        historia: "Principais Fatos: Rute, uma moabita, é conhecida por sua lealdade à sua sogra Noemi e por seu papel na linhagem de Davi e Jesus. Sua história é um exemplo de fé e devoção, e ela se casou com Boaz, que a redimiu e se tornou seu marido.Rute é a avó de Davi e ancestral de Jesus. Localização Bíblica: Livro de Rute.",
         link: "https://www.bibliaon.com/rute/",
         tags: "sogra moabita"
     },
     {
         personagem: "Debora",
-        historia: "Profetisa e juíza de Israel, liderou o povo em uma guerra vitoriosa contra os cananeus.",
+        historia: "Principais Fatos: Débora foi uma profetisa e juíza em Israel, destacando-se por sua liderança durante um período de opressão. Ela liderou Israel na batalha contra os cananeus e é conhecida por seu cântico de vitória junto com Baraque. Localização Bíblica: Juízes 4 e Juízes 5.",
         link:" https://www.bibliaon.com/juizes_4/",
         tags: "juíza profetisa"
     },
     {
         personagem: "Ester",
-        historia: "Uma judia que se tornou rainha da Pérsia e salvou seu povo do extermínio.",
+        historia: "Principais Fatos: Uma judia que se tornou rainha da Pérsia e salvou seu povo do extermínio. Ester, que se tornou rainha da Pérsia, desempenhou um papel crucial ao interceder pelo seu povo para evitar a destruição planejada pelo perverso Hamã. Sua coragem e estratégia salvaram os judeus da aniquilação. Localização Bíblica: Livro de Ester.",
         link : "https://www.bibliaon.com/ester/",
         tags: "rainha judia"
     },
     {
         personagem: "Maria", 
-        historia: "A mãe de Jesus, escolhida por Deus para ser a mãe do Messias.",
+        historia: "Principais Fatos: A mãe de Jesus, escolhida por Deus para ser a mãe do Messias. Maria foi  escolhida por Deus para conceber o Filho de Deus através do Espírito Santo. Sua aceitação da vontade de Deus e seu papel como mãe de Jesus são fundamentais na história cristã. Localização Bíblica: Lucas 1:26-38 e Mateus 1:18-25.",
         link: "https://www.bibliaon.com/lucas_1/",
         tags: "Jesus mãe"
     },
     {
         personagem: "Maria Madalena",
-        historia: "Uma das mulheres que seguiram Jesus e esteve presente em sua crucificação e ressurreição.",
+        historia: "Principais Fatos: Maria Madalena foi uma seguidora de Jesus, de quem foram expelidos sete demônios. Ela é conhecida por ter ungido o Senhor Jesus e ser a primeira pessoa a ver Jesus ressuscitado e anunciar a sua ressurreição aos discípulos. Localização Bíblica: Lucas 8:1-3 e João 20:1-18.",
        link: "https://www.bibliaon.com/joao_20/",
        tags: "mulheres  dicípula unguento"
     }     
